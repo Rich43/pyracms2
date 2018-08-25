1dfdb35222fdedbb193c098de5ad7a0861bcce29
Richie Ward <RichieS@GMail.com>
2018-08-25 11:06:31 +0100

    Cleanup model

--- pyracms2/models/model.py
+++ pyracms2/models/model.py
@@ -13,6 +13,31 @@ class Domain(Base):
     name = Column(Unicode)
 
 
+class UserGroup(Base):
+    __tablename__ = 'user_group'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    group_id = Column(Integer, ForeignKey('group.id'))
+
+
+class Translations(Base):
+    """
+    A collection of languages that you want to translate the translation to.
+    """
+    __tablename__ = 'translations'
+    id = Column(Integer, primary_key=True)
+    translations = relationship(Translation)
+
+
+class Group(Base):
+    __tablename__ = 'group'
+    id = Column(Integer, primary_key=True)
+    name = Column(Unicode)
+    display_name_id = Column(Integer, ForeignKey('translations.id'))
+    display_name = relationship(Translations, back_populates='translations')
+    user = relationship(UserGroup, back_populates='user')
+
+
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
@@ -28,29 +53,10 @@ class User(Base):
     group = relationship(UserGroup, back_populates='group')
 
 
-class Group(Base):
-    __tablename__ = 'group'
-    id = Column(Integer, primary_key=True)
-    name = Column(Unicode)
-    display_name_id = Column(Integer, ForeignKey('translations.id'))
-    display_name = relationship(Translations, back_populates='translations')
-    user = relationship(UserGroup, back_populates='user')
-
-
-class UserGroup(Base):
-    __tablename__ = 'user_group'
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    group_id = Column(Integer, ForeignKey('group.id'))
-
-
-class Translations(Base):
-    __tablename__ = 'translations'
-    id = Column(Integer, primary_key=True)
-    translations = relationship(Translation)
-
-
 class Translation(Base):
+    """
+    A translation for a particular language.
+    """
     __tablename__ = 'translation'
     id = Column(Integer, primary_key=True)
     translation_id = Column(Integer, ForeignKey('translations.id'))
@@ -58,19 +64,6 @@ class Translation(Base):
     display_name = Column(UnicodeText)
     description = Column(UnicodeText)
     locale = Column(LocaleType)
-
-
-class Entity(Base):
-    __tablename__ = 'entity'
-    id = Column(Integer, primary_key=True)
-    name = Column(Unicode)
-    route_name = Column(Unicode)
-    display_name_id = Column(Integer, ForeignKey('translations.id'))
-    display_name = relationship(Translations, back_populates='translations')
-    domain_id = Column(Integer, ForeignKey('domain.id'))
-    domain = relationship(Domain, back_populates='domain')
-    entity = relationship(EntityEntity, back_populates='entity')
-    translations = relationship(EntityTranslations, back_populates='entity')
 
 
 class EntityEntity(Base):
@@ -85,5 +78,19 @@ class EntityTranslations(Base):
     id = Column(Integer, primary_key=True)
     entity_id = Column(Integer, ForeignKey('entity.id'))
     translations_id = Column(Integer, ForeignKey('translations.id'))
+
+
+class Entity(Base):
+    __tablename__ = 'entity'
+    id = Column(Integer, primary_key=True)
+    name = Column(Unicode)
+    route_name = Column(Unicode)
+    display_name_id = Column(Integer, ForeignKey('translations.id'))
+    display_name = relationship(Translations, back_populates='translations')
+    domain_id = Column(Integer, ForeignKey('domain.id'))
+    domain = relationship(Domain, back_populates='domain')
+    entity = relationship(EntityEntity, back_populates='entity')
+    translations = relationship(EntityTranslations, back_populates='entity')
+
 
 # Index('my_index', MyModel.name, unique=True, mysql_length=255)
