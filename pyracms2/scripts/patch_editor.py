0a1e52742e8f5d9668af42b21c516e70f2099b37
Richie Ward <RichieS@GMail.com>
2018-11-10 18:53:56 +0000

    Identify sub parser

--- pyracms2/scripts/patch_editor.py
+++ pyracms2/scripts/patch_editor.py
@@ -1,7 +1,6 @@
 import argparse
 import sys
-
-from pyracms2.models import model
+from ..models import model
 
 SUB_COMMAND_HELP = 'sub-command --help'
 
@@ -12,11 +11,14 @@ def iter_sub_classes(cls):
 
 # noinspection PyProtectedMember
 def insert_add_sub_parser(sub_parser: argparse._SubParsersAction):
-    add_parser = sub_parser.add_parser('add')
-    add_help = " (Choose a table to add a row to)"
-    add_sub_parser = add_parser.add_subparsers(help=SUB_COMMAND_HELP + add_help)
+    root_add_parser = sub_parser.add_parser('add')
+    root_add_parser.set_defaults(sub_parser_1='add')
+    add_sub_parser = root_add_parser.add_subparsers(
+        help=SUB_COMMAND_HELP + " (Choose a table to add a row to)"
+    )
     for cls in iter_sub_classes(model.Base):
         add_parser = add_sub_parser.add_parser(cls)
+        add_parser.set_defaults(sub_parser_2=cls)
         db_cls = getattr(model, cls)
         attributes = db_cls.__table__.columns.keys()
         for attribute in attributes:
@@ -27,4 +29,5 @@ def main():
     root_parser = argparse.ArgumentParser()
     root_sub_parser = root_parser.add_subparsers(help=SUB_COMMAND_HELP)
     insert_add_sub_parser(root_sub_parser)
-    root_parser.parse_args(None if sys.argv[1:] else ['-h'])
+    args = root_parser.parse_args(None if sys.argv[1:] else ['-h'])
+    print(args)
